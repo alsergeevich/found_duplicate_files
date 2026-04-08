bc2f3197f2670980c219b52d32280c7c017f59a9
alsergeevich <alsergeevich@gmail.com>
2026-04-08 22:17:35 +0500

    Please provide the code changes or file diffs you  would like me to summarize.

--- src/group.rs
+++ src/group.rs
@@ -1,10 +1,9 @@
 use std::collections::{HashMap, VecDeque};
 use std::fs;
 use std::path::PathBuf;
-use rayon::iter::IntoParallelIterator;
+use rayon::iter::{IntoParallelIterator, ParallelBridge};
 use rayon::iter::ParallelIterator;
 use crate::md5::*;
-use std::sync::Mutex;
 //функция нулевого шага, обходит все папки в дереве и собирает все файлы
 pub fn directory_traversal(path: PathBuf) -> Result<Vec<PathBuf>, std::io::Error> {
     let mut vec_dirs = VecDeque::new();
@@ -107,22 +106,52 @@ pub fn group_files_by_partial_hash(group_by_size: HashMap<u64, Vec<PathBuf>>) ->
     Ok(map_files_by_hash)
 }
 //функция третьего шага, группирует файлы по полному хешу
-pub fn group_files_by_full_hash(group_by_partial_hash: HashMap<(u64, String), Vec<PathBuf>>) -> Result<HashMap<String, Vec<PathBuf>>, std::io::Error> {
-    let all_files: Vec<PathBuf> = group_by_partial_hash.into_values().flat_map(|paths| paths.into_iter()).collect(); //проходим по всей group_by_partial_hash и собираем все файлы в один вектор
-    let mut map_files_by_hash = Mutex::new(HashMap::new()); //тут можно обойтись без size, оборачиваем в мютекс
+// pub fn group_files_by_full_hash(group_by_partial_hash: HashMap<(u64, String), Vec<PathBuf>>) -> Result<HashMap<String, Vec<PathBuf>>, std::io::Error> {
+//     let all_files: Vec<PathBuf> = group_by_partial_hash.into_values().flat_map(|paths| paths.into_iter()).collect(); //проходим по всей group_by_partial_hash и собираем все файлы в один вектор
+//     let mut map_files_by_hash = Mutex::new(HashMap::new()); //тут можно обойтись без size, оборачиваем в мютекс
     
-    all_files.into_par_iter()
-        .for_each(|path| {
-            match full_hash_file(&path) {
-                Ok(hash) => {
-                    map_files_by_hash.lock().unwrap().entry(hash).or_insert_with(Vec::new).push(path);
-                    }
-                    Err(e) => {
-                        eprintln!("Ошибка вычисления хеша для файла {:?} : {}", path, e);
-                    }
-                }
+//     all_files.into_par_iter()
+//         .for_each(|path| {
+//             match full_hash_file(&path) {
+//                 Ok(hash) => {
+//                     map_files_by_hash.lock().unwrap().entry(hash).or_insert_with(Vec::new).push(path);
+//                     }
+//                     Err(e) => {
+//                         eprintln!("Ошибка вычисления хеша для файла {:?} : {}", path, e);
+//                     }
+//                 }
+//         });
+//     let mut map_files_by_hash = map_files_by_hash.into_inner().unwrap();
+//     map_files_by_hash.retain(|_, paths| paths.len() > 1);  //удаляем все записи где только один файл
+//     Ok(map_files_by_hash)
+// }
+pub fn group_files_by_full_hash(mut group_by_partial_hash: HashMap<(u64, String), Vec<PathBuf>>) -> Result<HashMap<String, Vec<PathBuf>>, std::io::Error> {
+    //после того как мы сгруппировали файлы по частичному хешу, проверим есть ли среди них одиночные файлы и отсеем их
+    group_by_partial_hash.retain(|_, paths| paths.len() > 1); //удаляем все записи где только один файл
+    //используем вместо mutex map-reduce (fold/reduce)
+    let final_map = group_by_partial_hash
+        .into_values()
+        .flatten()
+        .par_bridge()
+        .fold(|| HashMap::<String, Vec<PathBuf>>::new(), |mut acc: HashMap<String, Vec<PathBuf>>, path: PathBuf| {
+            if let Ok(hash) = full_hash_file(&path) {
+                acc.entry(hash).or_default().push(path);
+            }
+            acc
+        },
+        )
+        .reduce(|| HashMap::new(), |mut map1: HashMap<String, Vec<PathBuf>>, map2: HashMap<String, Vec<PathBuf>>| {   //объединяем карты
+            for (hash, paths) in map2 {
+                map1.entry(hash).or_default().extend(paths);
+            }
+            map1
         });
-    let mut map_files_by_hash = map_files_by_hash.into_inner().unwrap();
-    map_files_by_hash.retain(|_, paths| paths.len() > 1);  //удаляем все записи где только один файл
-    Ok(map_files_by_hash)
+        let mut result = final_map;
+        result.retain(|_, paths: &mut Vec<PathBuf>| paths.len() > 1);  //удаляем все записи где только один файл
+        Ok(result)
+
+
+    
+    
+    
 }
